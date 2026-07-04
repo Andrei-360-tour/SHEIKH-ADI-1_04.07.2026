e6dd121ce993b6f83623d85c673152d9c0a2c1e5
Andrei-360-tour <156935416+Andrei-360-tour@users.noreply.github.com>
2026-07-04 15:20:02 +0300

    Commit

---
+++ data.js
@@ -0,0 +1,87 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-overviev1",
+      "name": "OVERVIEV1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.39087948714436394,
+        "pitch": 0.29227912371723264,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.6537986017040982,
+          "pitch": 0.25622827090638367,
+          "rotation": 0,
+          "target": "1-overviev2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-overviev2",
+      "name": "OVERVIEV2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0976459102435,
+        "pitch": 0.18824757120771807,
+        "fov": 1.3412181875493232
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.7237526840190007,
+          "pitch": -0.012618824618970237,
+          "rotation": 0,
+          "target": "0-overviev1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "SH-1_04.07.2026",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
